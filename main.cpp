1bc649ca19d2310417aecc0f03dee780a6668756
Enrico <enricofen300@gmail.com>
2022-03-16 13:32:08 +0100

    prime idee

--- main.cpp
+++ main.cpp
@@ -1,12 +1,62 @@
+class Key
+{
+protected:
+    int number = -1;
+
+public:
+    int getNumber()
+    {
+        number += 1;
+        return number;
+    }
+
+}
+
+class Stanza
+{
+protected:
+    Key key; // numero univoco
+    listaOggetti; //all'interno c'è listaNemici listaTesori....
+
+public:
+    int getKey()
+    {
+        return key;
+    }
+    void init()
+    {
+        key = key.getNumber();
+    }
+}
+
 #include <ncurses.h>
 
-int main()
+struct listaStanze
 {
-    initscr();                 /* Start curses mode 		  */
-    printw("Hello World !!!"); /* Print Hello World		  */
-    refresh();                 /* Print it on to the real screen */
-    getch();                   /* Wait for user input */
-    endwin();                  /* End curses mode		  */
+    Stanza stanza;
+    listaStanze *porta1;
+    listaStanze *porta2;
+    listaStanze *porta3;
+    listaStanze *prev;
+};
 
+listaStanze createListStanze()
+{
+    // crea lista con key univoca
+    stanza = stanza.init();
+}
+
+main()
+{
+    listaStanze myLista createListStanze();
+
+    WINDOW stanza1;
+    initscr(g);
+
+    /*initscr();                 /* Start curses mode 		  */
+    // printw("Hello World !!!"); /* Print Hello World		  */
+    // refresh();                 /* Print it on to the real screen */
+    // getch();                   /* Wait for user input */
+    // endwin();                  /* End curses mode		  */
     return 0;
 }
