391c9afb2f72bd77a09026d941769f397e57785a
annalisa <annapol2003@gmail.com>
2022-03-16 14:56:58 +0100

    prova

--- main.cpp
+++ main.cpp
@@ -1,63 +1,11 @@
-class Key
-{
-protected:
-    int number = -1;
-
-public:
-    int getNumber()
-    {
-        number += 1;
-        return number;
-    }
-
-}
-//prova 2
-
-class Stanza
-{
-protected:
-    Key key; // numero univoco
-    listaOggetti; //all'interno c'è listaNemici listaTesori....
-
-public:
-    int getKey()
-    {
-        return key;
-    }
-    void init()
-    {
-        key = key.getNumber();
-    }
-}
-
 #include <ncurses.h>
 
-struct listaStanze
+int main()
 {
-    Stanza stanza;
-    listaStanze *porta1;
-    listaStanze *porta2;
-    listaStanze *porta3;
-    listaStanze *prev;
-};
-
-listaStanze createListStanze()
-{
-    // crea lista con key univoca
-    stanza = stanza.init();
-}
-
-main()
-{
-    listaStanze myLista createListStanze();
-
-    WINDOW stanza1;
-    initscr(g);
-
-    /*initscr();                 /* Start curses mode 		  */
-    // printw("Hello World !!!"); /* Print Hello World		  */
-    // refresh();                 /* Print it on to the real screen */
-    // getch();                   /* Wait for user input */
-    // endwin();                  /* End curses mode		  */
+    initscr();                 /* Start curses mode 		  */
+    printw("Hello World !!!"); /* Print Hello World		  */
+    refresh();                 /* Print it on to the real screen */
+    getch();                   /* Wait for user input */
+    endwin();                  /* End curses mode		  */
     return 0;
 }
---
+++ prove.cpp
@@ -0,0 +1,49 @@
+/*class Key
+{
+protected:
+    int number = -1;
+
+public:
+    int getNumber()
+    {
+        number += 1;
+        return number;
+    }
+
+}
+//prova 2
+
+class Stanza
+{
+protected:
+    Key key; // numero univoco
+    listaOggetti; //all'interno c'è listaNemici listaTesori....
+
+public:
+    int getKey()
+    {
+        return key;
+    }
+    void init()
+    {
+        key = key.getNumber();
+    }
+}
+*/
+/*struct listaStanze
+{
+    Stanza stanza;
+    listaStanze *porta1;
+    listaStanze *porta2;
+    listaStanze *porta3;
+    listaStanze *prev;
+};
+
+listaStanze createListStanze()
+{
+    // crea lista con key univoca
+    stanza = stanza.init();
+}
+*/
+//make main
+// ./main
