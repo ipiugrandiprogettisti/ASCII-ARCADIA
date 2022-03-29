ffbf6eaf3b8ac3439a396ac792a63d44b80c4ddb
Enrico <enricofen300@gmail.com>
2022-03-29 23:03:03 +0200

    menu is working

--- game.cpp
+++ game.cpp
@@ -4,6 +4,32 @@
 #include "header/game.hpp"
 #include "header/utils.hpp"
 
+// sel is selected item to highlight
+void printMenu(int sel, int totItems, char menuItems[][20])
+{
+    int halfY = LINES / 2 - 1;
+    int halfX = COLS / 2 - 1;
+    int padding = 0; // set padding between menu items when displayed
+    for (int i = 0; i < totItems; i++)
+    {
+
+        // check if current item is the selected one, if it is like so print it glowly
+        if (i == sel)
+        {
+            attron(COLOR_PAIR(2));
+            mvaddstr(halfY + padding, halfX, menuItems[i]);
+            attroff(COLOR_PAIR(2));
+        }
+        else
+        {
+            attron(COLOR_PAIR(1));
+            mvaddstr(halfY + padding, halfX, menuItems[i]);
+            attroff(COLOR_PAIR(1));
+        }
+        padding += 3;
+    }
+}
+
 /*
 draw the main menu and returns choice taken by user:
 - 0: Play
@@ -15,7 +41,7 @@ int getMenu(int maxY, int maxX, int offY, int offX)
               MIN_ITEMS = 0,
               MAX_LENGTH_ITEM = 20;
     int width, height, halfX, halfY;
-    char string[MAX_ITEMS][MAX_LENGTH_ITEM] = {"Play", "Credits"};
+    char menuItems[MAX_ITEMS][MAX_LENGTH_ITEM] = {"Play", "Credits"};
     int selectedItem = 0; // could only be -1, 0 or 1. 0 is default
 
     WINDOW *myWin = newwin(maxY, maxX, offY, offX);
@@ -37,28 +63,8 @@ int getMenu(int maxY, int maxX, int offY, int offX)
     mvaddstr(startY, startY, "ciao a tutti");
     attroff(COLOR_PAIR(1));
     */
-    // center string
-    halfY = LINES / 2 - 1;
-    halfX = COLS / 2 - 1;
-    int padding = 0; // set padding between menu items when displayed
-    for (int i = 0; i < MAX_ITEMS; i++)
-    {
 
-        // check if current item is the selected one, if it is like so print it glowly
-        if (i == selectedItem)
-        {
-            attron(COLOR_PAIR(2));
-            mvaddstr(halfY + padding, halfX, string[i]);
-            attroff(COLOR_PAIR(2));
-        }
-        else
-        {
-            attron(COLOR_PAIR(1));
-            mvaddstr(halfY + padding, halfX, string[i]);
-            attroff(COLOR_PAIR(1));
-        }
-        padding += 3;
-    }
+    printMenu(selectedItem, MAX_ITEMS, menuItems);
 
     int ch; // pressed key
     MyString item = MyString();
@@ -68,36 +74,43 @@ int getMenu(int maxY, int maxX, int offY, int offX)
         switch (ch)
         {
         case KEY_UP:
-            move(0, 0);
             clrtoeol();
             /*
+            clrtoeol():
             erase the current line to the right of the cursor,
             inclusive, to the end of the current line.
             Blanks created by erasure have the current background
             rendition (as set by wbkgdset) merged into them.
             */
-            wrefresh(myWin);
-            if (selectedItem + 1 > MAX_ITEMS - 1)
-                selectedItem = MIN_ITEMS; // reset selectedItem to 0
+            if (selectedItem + 1 >= MAX_ITEMS)
+                selectedItem = MIN_ITEMS; // reset selectedItem to MIN_ITEMS (0)
             else
                 selectedItem++;
+            printMenu(selectedItem, MAX_ITEMS, menuItems);
+            /*
+            move(0, 0);
             item.reset();
             item.append(itoa(selectedItem));
             addstr(item.get());
+            */
+            wrefresh(myWin);
             break;
 
         case KEY_DOWN:
-            move(0, 0);
             clrtoeol();
             wrefresh(myWin);
-            if (selectedItem - 1 < MIN_ITEMS - 1)
-                selectedItem = MAX_ITEMS; // reset selectedItem to 0
+            if (selectedItem - 1 < MIN_ITEMS)
+                selectedItem = MAX_ITEMS - 1; // reset selectedItem to MAX_ITEMS (2) //Forse, fixare
             else
                 selectedItem--;
-
+            printMenu(selectedItem, MAX_ITEMS, menuItems);
+            /*
+            move(0, 0);
             item.reset();
             item.append(itoa(selectedItem));
             addstr(item.get());
+            */
+            wrefresh(myWin);
             break;
         }
     }
