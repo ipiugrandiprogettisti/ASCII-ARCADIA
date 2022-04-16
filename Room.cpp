902ba6be025487168e5dc52ec8e7c040557da4b0
Enrico <enricofen300@gmail.com>
2022-04-16 15:12:03 +0200

    fixed room

--- Room.cpp
+++ Room.cpp
@@ -4,48 +4,6 @@
 #include "header/MyString.hpp"
 #include "header/utils.hpp"
 
-//  draws a box with custom corners, SX / RX are the north corners, sx / rx are the south corners.
-/*void customBox(WINDOW *win, int roomHeigth, int roomWidth, int winHeigth, int winWidth, chtype hLine, chtype vLine)
-{
-    int halfY = winWidth / 2;
-    int halfX = winHeigth / 2;
-    int adjWidth = (winWidth - roomWidth) / 2;    // adjusted width
-    int adjHeigth = (winHeigth - roomHeigth) / 2; // adjusted heigth
-
-    for (int i = 0; i < roomWidth; i++)
-    {
-        if (i == 0 || i == roomWidth)
-            mvwaddch(win, halfY - roomHeigth / 2, adjWidth + i, ACS_PLUS);
-        else
-            mvwaddch(win, 20, adjWidth + i, ACS_HLINE);
-    }
-    for (int i = 0; i < roomWidth; i++)
-    {
-        if (i == 0 || i == roomWidth)
-            mvwaddch(win, halfY + roomHeigth / 2, adjWidth + i, ACS_PLUS);
-
-        else
-            mvwaddch(win, halfY + roomHeigth / 2, adjWidth + i, ACS_HLINE);
-    }
-
-    // est side
-    /*for (int i = 0; i < roomHeigth; i++)
-    {
-        if (i == 0 || i == roomHeigth - 1)
-            mvwaddch(win, adjHeigth + i, adjWidth + width, ACS_PLUS);
-        else
-            mvwaddch(win, adjHeigth + i, adjWidth + width, ACS_VLINE);
-    }
-    // ovest side
-    for (int i = 0; i < roomHeigth; i++)
-    {
-        if (i == 0 || i == roomHeigth - 1)
-            mvwaddch(win, adjHeigth + i, adjWidth, ACS_PLUS);
-        else
-            mvwaddch(win, adjHeigth + i, adjWidth, ACS_VLINE);
-    }
-}*/
-
 // Constructor
 Room ::Room()
 {
@@ -105,20 +63,63 @@ void Room::draw(int maxCols, int maxLines)
     int adjWidth = halfX - roomWidth / 2;            // adjusted width
     int adjHeigth = halfY - roomHeigth / 2;          // adjusted heigth
     int offY = roomHeigth / 2, offX = roomWidth / 4; // offset; useful to center box
-    start_color();
-    init_pair(1, COLOR_GREEN, COLOR_BLUE);
-    init_pair(2, COLOR_GREEN, COLOR_YELLOW);
 
     win = newwin(roomHeigth, roomWidth, offY, offX); // create a CENTERED box
-    
-    wbkgd(stdscr, COLOR_PAIR(1));
 
     //  create the ROOM (box)
-    //wborder(win,);
-
-    //customBox(win, roomHeigth - 2, roomWidth - 2, maxLines, maxCols, ACS_HLINE, ACS_VLINE);
+    /*
+    int wborder(WINDOW *win, chtype ls, chtype rs, chtype ts, chtype bs, chtype tl, chtype tr, chtype bl, chtype br);
+    The argument ls is a character and attributes used for the left side of the border, rs right side, ts - top side,
+    bs - bottom side, tl - top left-hand corner, tr - top right-hand corner, bl - bottom left-hand corner,
+    and br - bottom right-hand corner.
+    If arguments is 0, then is showed default ACS
+    */
+    wborder(win, 0, 0, 0, 0, 0, 0, 0, 0);
+    // mvwhline(win, 0, 0, ACS_BLOCK, 5);
+    // mvwvline(win, 0, 0, ACS_CKBOARD, 5);
     // wbkgd(win, COLOR_PAIR(1));
 }
 
 // funzione prova
 // customBox(roomWin, heigth, width, ACS_HLINE, ACS_VLINE, ACS_UARROW, ACS_RARROW, ACS_DARROW, ACS_LARROW);
+//  draws a box with custom corners, SX / RX are the north corners, sx / rx are the south corners.
+/*void customBox(WINDOW *win, int roomHeigth, int roomWidth, int winHeigth, int winWidth, chtype hLine, chtype vLine)
+{
+    int halfY = winWidth / 2;
+    int halfX = winHeigth / 2;
+    int adjWidth = (winWidth - roomWidth) / 2;    // adjusted width
+    int adjHeigth = (winHeigth - roomHeigth) / 2; // adjusted heigth
+
+    for (int i = 0; i < roomWidth; i++)
+    {
+        if (i == 0 || i == roomWidth)
+            mvwaddch(win, halfY - roomHeigth / 2, adjWidth + i, ACS_PLUS);
+        else
+            mvwaddch(win, 20, adjWidth + i, ACS_HLINE);
+    }
+    for (int i = 0; i < roomWidth; i++)
+    {
+        if (i == 0 || i == roomWidth)
+            mvwaddch(win, halfY + roomHeigth / 2, adjWidth + i, ACS_PLUS);
+
+        else
+            mvwaddch(win, halfY + roomHeigth / 2, adjWidth + i, ACS_HLINE);
+    }
+
+    // est side
+    /*for (int i = 0; i < roomHeigth; i++)
+    {
+        if (i == 0 || i == roomHeigth - 1)
+            mvwaddch(win, adjHeigth + i, adjWidth + width, ACS_PLUS);
+        else
+            mvwaddch(win, adjHeigth + i, adjWidth + width, ACS_VLINE);
+    }
+    // ovest side
+    for (int i = 0; i < roomHeigth; i++)
+    {
+        if (i == 0 || i == roomHeigth - 1)
+            mvwaddch(win, adjHeigth + i, adjWidth, ACS_PLUS);
+        else
+            mvwaddch(win, adjHeigth + i, adjWidth, ACS_VLINE);
+    }
+}*/
