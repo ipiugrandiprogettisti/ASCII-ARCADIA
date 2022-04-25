7b743c157c8f1c1527e6ea0583f33f92c7cd9ab8
FRA <menarinif@gamil.com>
2022-04-25 12:34:50 +0200

    character

--- Character.cpp
+++ Character.cpp
@@ -1,5 +1,43 @@
-#include "Character.hpp"
+#include "header/Character.hpp"
 
-class Character: public Entity {
-    
-};
+// constructor
+Character::Character(char name[], int current_life, int max_life, int atk_damage)
+{
+    strcpy(this->name, name);
+    this->current_life = current_life;
+    this->max_life = max_life;
+    this->atk_damage = atk_damage;
+}
+
+// ritorna la vita corrente
+int Character::getLife()
+{
+    return current_life;
+}
+
+// prende in input il danno ricevuto e aggiorna la vita sottraendoglielo
+void Character::takeDamage(int damage_received)
+{
+    int curr_life = getLife();
+    if ((curr_life - damage_received) >= 0)
+    {
+        this->current_life = (curr_life - damage_received);
+    }
+    else
+    {
+        this->current_life = 0;
+    }
+}
+
+void Character::increaseLife(int healing)
+{
+    int curr_life = getLife();
+    if (curr_life + healing <= this->max_life)
+    {
+        this->current_life = (curr_life + healing);
+    }
+    else
+    {
+        this->current_life = this->max_life;
+    }
+}
--- header/Character.hpp
+++ header/Character.hpp
@@ -1,6 +1,30 @@
-
+#include <iostream>
+#include <cstring>
 #include "Entity.hpp"
 
+#define L 30
 class Character : public Entity
 {
+protected:
+    char name[L];
+    int current_life;
+    int max_life;
+    int atk_damage; // danno che produce in attacco
+
+public:
+    // constructor
+    Character();
+
+    // constructor
+    Character(char name[], int current_life, int max_life, int atk_damage);
+
+    // ritorna la vita corrente
+    int getLife();
+
+    // prende in input il danno ricevuto e aggiorna la vita sottraendoglielo
+    void takeDamage(int);
+
+    //aumenta la vita in base agli artefatti
+    void increaseLife(int);
+
 };
