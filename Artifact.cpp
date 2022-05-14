22678319189ec53c3e794d06ddf6fbcb11d6a505
annalisapol <annapol2003@gmail.com>
2022-05-14 17:03:20 +0200

    added symbols to artifacts

--- Artifact.cpp
+++ Artifact.cpp
@@ -11,7 +11,7 @@ Artifact ::Artifact()
     tag = 111;
     position.y = 0;
     position.x = 0;
-};
+}
 
 Artifact ::Artifact(char name[NMAX], int rarity, int posy, int posx, chtype tagg) : Entity(position.y, position.x, tag)
 {
@@ -21,7 +21,7 @@ Artifact ::Artifact(char name[NMAX], int rarity, int posy, int posx, chtype tagg
     this->position.x = posx;
     this->tag = tagg;
     this->lifepoints = 0;
-};
+}
 
 int Artifact ::getlifep(Artifact p)
 {
@@ -33,9 +33,9 @@ int Artifact ::getrarity(Artifact p)
     return p.rarity;
 }
 
-void Artifact ::set_lifep(Artifact p, int prarity)
+void Artifact ::set_lifep(Artifact p)
 {
-    prarity = p.rarity;
+    int prarity = p.rarity;
     switch (prarity)
     {
     case 1:
@@ -55,9 +55,38 @@ void Artifact ::set_lifep(Artifact p, int prarity)
     }
 }
 
+void Artifact ::set_symbol(Artifact p)
+{
+    int prarity = p.rarity;
+    switch (prarity)
+    {
+    case '1':
+        p.tag = 169;
+    case '2':
+        p.tag = 174;
+    case '3':
+        p.tag = 248;
+    case '4':
+        p.tag = 216;
+    case '5':
+        p.tag = 163;
+    case '6':
+        p.tag = 165;
+    case '7':
+        p.tag = 167;
+    }
+}
+
 void Artifact ::gainlife(Character a, Artifact p)
 {
     int actual = a.current_life;
     int heal = p.lifepoints;
     a.current_life = actual + heal;
-};
+}
+
+void Artifact ::printArtifact(Artifact p, WINDOW *w)
+{
+    int py = p.position.y;
+    int px = p.position.x;
+    mvwaddch(w, py, px, p.tag);
+}
--- header/Artifact.hpp
+++ header/Artifact.hpp
@@ -2,13 +2,13 @@
 #include "Character.hpp"
 
 #define NMAX 30
-#define COMMON 1     // 1 pt vita
-#define RARE 2       // 3 pt vita
-#define VERY_RARE 3  // 5 pt vita
-#define SPECIAL 4    // 7 pt vita
-#define EPIC 5       // 10 pt vita
-#define LEGENDARY 6, // 15 pt vita
-#define MYTHIC 7     // 20 pt vita
+#define COMMON 1     // 1 pt vita, simbolo ascii 169 "©"
+#define RARE 2       // 3 pt vita, simbolo ascii 174 "®"
+#define VERY_RARE 3  // 5 pt vita, simbolo ascii 248 "ø"
+#define SPECIAL 4    // 7 pt vita, simbolo ascii 216 "Ø"
+#define EPIC 5       // 10 pt vita, simbolo ascii 163 "£"
+#define LEGENDARY 6, // 15 pt vita, simbolo ascii 165 "¥"
+#define MYTHIC 7     // 20 pt vita, simbolo ascii 167 "§"
 // i could difference between consumable and collectables
 //  classe artefatto -> da vita etc
 class Artifact : public Entity
@@ -28,7 +28,12 @@ public:
     // returns artifact rarity
     int getrarity(Artifact p);
     // sets artifact lifepoints based on rarity
-    void set_lifep(Artifact p, int prarity);
-    // resets character life based on artifact healing
+    void set_lifep(Artifact p);
+    // sets artifact symbol based on rarity
+    void set_symbol(Artifact p);
+
+        // resets character life based on artifact healing
     void gainlife(Character a, Artifact p);
+
+    void printArtifact(Artifact p, WINDOW *w);
 };
