e8f6f5f824b81e9258a358a1c415ce1a6866496e
annalisapol <annapol2003@gmail.com>
2022-07-18 16:28:03 +0200

    added symbols to powers

--- header/Power.hpp
+++ header/Power.hpp
@@ -1,8 +1,8 @@
 #include "Entity.hpp"
 
-// classe potere -> sblocca passaggi
-// potere 1 -> apre porta
-// potere 2 -> aumenta i proiettili di uno
+// classe potere 1-> sblocca passaggi
+// potere 1 -> apre porta                       chtype 230 : æ
+// potere 2 -> aumenta num proiettili di uno    chtype 187 : »
 class Power : public Entity
 {
 protected:
@@ -13,6 +13,7 @@ public:
 
     Power(int type, int y, int x, chtype tag);
 
+    void setPower(Power p);
     int getType(Power p);
 
     void printPower(Power p, WINDOW *w);
