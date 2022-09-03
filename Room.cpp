1327e0c9fa289576658ec50a75a1e0d780755683
annalisapol <annapol2003@gmail.com>
2022-09-03 12:27:38 +0200

    last fix
    last fix

--- Room.cpp
+++ Room.cpp
@@ -199,7 +199,7 @@ void Room::place_enemies(bool b)
         }
     }
 
-    //FIXME: qui va funzione per aggiungere enemy en alla lista
+    // FIXME: qui va funzione per aggiungere enemy en alla lista
 }
 
 // random path generator
@@ -247,7 +247,6 @@ void Room::randomPathWall(pos position, int h, int w)
         }
     }
 }
-
 
 // Constructor
 Room ::Room()
@@ -457,7 +456,6 @@ bool Room::setUp(int maxCols, int maxLines, struct door myDoor)
         int h = (rand() % (MAXWALLHEIGTH)) + 1; // random number of walls
         int w = (rand() % (MAXWALLWIDTH)) + 1;  // random number of walls
 
-        
         pos pos1;
         pos1.x = posX;
         pos1.y = posY;
@@ -663,9 +661,9 @@ void Room::aBullMov(Protagonist &P, bullet &b)
             if (tmpnem->B.bulletpos.y == next.y && tmpnem->B.bulletpos.x == next.x)
             {
                 flag = true;
-                this->placeObject(now, ' ');
+                this->placeObject(now, empty);
                 P.bulletRemove(b);
-                this->placeObject(next, ' ');
+                this->placeObject(next, empty);
                 this->bullet_enemyRemove(tmpnem->B);
                 this->drawLook();
                 wrefresh(this->getWindow());
@@ -673,6 +671,12 @@ void Room::aBullMov(Protagonist &P, bullet &b)
             }
             tmpnem = tmpnem->next;
         }
+    }
+    else if (nextP == ACS_BULLET)
+    {
+
+        this->placeObject(now, empty);
+        P.bulletRemove(b);
     }
     else if (nextP == 'P') // PROIETTILE -> POTERE
     {
